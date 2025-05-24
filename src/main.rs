cfb233e33433448f496c94b0b85c68d5d3f4097d
Antonio <antoniofilhojsa@gmail.com>
2025-05-24 12:34:44 -0300

    add lib file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+use minigrep::Config;
 
 
 fn main() {
@@ -16,31 +15,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config){
+    if let Err(e) = minigrep::run(config){
         println!("Error: {}", e);
         process::exit(1);
     }
-}
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let content = fs::read_to_string(config.filename)?;
-    println!("File content: {}", content);
-
-    Ok(())
-}
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config{
-    fn new(args: &[String]) -> Result<Config, &str>{
-        if args.len() < 3{
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config { query, filename })
-    }
-
 }
